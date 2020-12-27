0f31d83cf1036c513d12c37fd1686ea4134c29b3
Niqiao-tong <1257224568@qq.com>
2020-12-27 13:18:04 +0800

    第一次提交嵌入

---
+++ WindowsFormsApplication1/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("User32.dll", EntryPoint = "SetParent")]
+        private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll", EntryPoint = "ShowWindow")]
+        private static extern int ShowWindow(IntPtr hwnd, int nCmdShow);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+
+            string fexePath = ofd.FileName; // 外部exe位置
+
+            Process p = new Process();
+            p.StartInfo.FileName = fexePath;
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            p.Start();
+
+            while (p.MainWindowHandle.ToInt32() == 0)
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+            SetParent(p.MainWindowHandle, this.panel1.Handle);
+            ShowWindow(p.MainWindowHandle, (int)ProcessWindowStyle.Normal);
+        }
+    }
+}
